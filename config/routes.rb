b6b3e9e44ca6addb245495a97bcffb8b3459e3af
kuimac <lason123>
2015-11-09 19:32:52 +0900

    Finish layout and routes (5 end)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get  'static_pages/help'
-  get  'static_pages/about'
-  get  'static_pages/contact'
+
+  get 'help' => 'static_pages#help'
+  #get  'static_pages/help'
+  get 'about' => 'static_pages#about'
+  #get  'static_pages/about'
+  get 'contact' => 'static_pages#contact'
+  #get  'static_pages/contact'
+
+  get 'signup' => 'user#new'
+  #get 'user/new'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
