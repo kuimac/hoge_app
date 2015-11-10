82f384385d6e125759c05d7879dc21229061a68f
kuimac <lason123>
2015-11-11 00:52:14 +0900

    next 6.2.5 一意性を検証する

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-#リスト6.5: 有効なUserかどうかをテストする GREEN
+
   def setup
     @user = User.new(name: "Example User", email: "user@example.com")
   end
@@ -9,23 +9,17 @@ class UserTest < ActiveSupport::TestCase
   test "should be valid" do
     assert @user.valid?
   end
-#リスト6.5: 有効なUserかどうかをテストする GREEN
 
-#リスト6.7: name属性にバリデーションに対するテスト
   test "name should be present" do
     @user.name = "  "
     assert_not @user.valid?
   end
-  #リスト6.7: name属性にバリデーションに対するテスト
 
-  #リスト6.11: email属性の検証に対するテスト
   test "email should be present" do
     @user.email = " "
     assert_not @user.valid?
   end
-  #リスト6.11: email属性の検証に対するテスト
 
-  #リスト6.14: nameの長さの検証に対するテスト
   test "name should not be too long" do
     @user.name = "a" * 51
     assert_not @user.valid?
@@ -35,6 +29,23 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
   end
-  #リスト6.14: nameの長さの検証に対するテスト
 
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+  
 end
