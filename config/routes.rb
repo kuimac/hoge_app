e940caeba3142ce95ae96170aaaf0192035f030b
kuimac <lason123>
2015-11-15 23:59:28 +0900

    next 8.2.2 現在のユーザー

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
 
-  get 'help' => 'static_pages#help'
+  root                'static_pages#home'
+
+  get    'help'    => 'static_pages#help'
   #get  'static_pages/help'
-  get 'about' => 'static_pages#about'
+  get    'about'   => 'static_pages#about'
   #get  'static_pages/about'
-  get 'contact' => 'static_pages#contact'
+  get    'contact' => 'static_pages#contact'
   #get  'static_pages/contact'
-  get 'signup' => 'user#new'
+  get    'signup'  => 'user#new'
   #get 'user/new'
 
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   resources :users, :controller => :user
 
 
