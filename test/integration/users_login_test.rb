21a596ba6a18b01eb11e20fdbfe31527a683413a
kuimac <lason123>
2015-11-18 15:30:43 +0900

    a

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -56,6 +56,6 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test "login without remembering" do
     log_in_as(@user, remember_me: '0')
-    assert_nil cookies[:remember_token] #'remember_token'
+    assert_nil cookies[`remember_token`]
   end
 end
