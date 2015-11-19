e3ebd260609930dba7a9bcebee3d1aee707b817a
kuima <kumagaikousuke1995@gmail.com>
2015-11-20 05:12:45 +0900

    next 9.2.2 正しいユーザーを要求する

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,5 @@
 class UserController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -41,4 +42,13 @@ class UserController < ApplicationController
                                    :password_confirmation)
     end
 
+    # Before filters
+
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 end
