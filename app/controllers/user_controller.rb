3c1cda251dfc4ae40fa1ec6fb7a9ce4afea7837a
kuimac <lason123>
2015-12-04 13:06:06 +0900

    a

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -70,7 +70,7 @@ class UserController < ApplicationController
       redirect_to(root_url) unless current_user?(@user)
     end
 
-    
+
     # Confirms an admin user.
         def admin_user
           redirect_to(root_url) unless current_user.admin?
