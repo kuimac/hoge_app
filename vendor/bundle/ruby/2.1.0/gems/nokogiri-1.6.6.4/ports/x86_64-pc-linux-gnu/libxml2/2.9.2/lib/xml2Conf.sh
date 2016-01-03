abf79abda17c6f9df1b14c4f89e6086f8b15ee30
kuima <kumagaikousuke1995@gmail.com>
2016-01-03 18:50:30 +0900

    a

--- vendor/bundle/ruby/2.1.0/gems/nokogiri-1.6.6.4/ports/x86_64-pc-linux-gnu/libxml2/2.9.2/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/home/ubuntu/Documents/RoR/apple/hoge_app/vendor/bundle/ruby/2.1.0/gems/nokogiri-1.6.6.4/ports/x86_64-pc-linux-gnu/libxml2/2.9.2/lib"
-XML2_LIBS="-lxml2 -lz   -lm "
-XML2_INCLUDEDIR="-I/home/ubuntu/Documents/RoR/apple/hoge_app/vendor/bundle/ruby/2.1.0/gems/nokogiri-1.6.6.4/ports/x86_64-pc-linux-gnu/libxml2/2.9.2/include/libxml2"
-MODULE_VERSION="xml2-2.9.2"
-
